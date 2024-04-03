5f3442496b4063bd28f7d8a34ee0239bb4c9d597
Your Name <your.email@example.com>
2024-04-04 02:15:38 +0500

    updated

--- index.js
+++ index.js
@@ -2,28 +2,57 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
 console.log(chalk.red.bold.italic(`
-**********
-TO DO LIST
-**********`));
+        **********
+        TODO LIST
+        **********`));
 let todoArray = [];
 while (true) {
     let answer = await inquirer.prompt([
         {
-            name: "select",
             type: 'list',
-            message: "WELLCOME\n 'TO DO LIST' \n  what whould you like to do? \n Enter 'new' to add in list. \n 'list' to display list. \n Enter 'delete to delete from list.\n Enter 'delete to delete from list.\m Enter 'quit' to close the application",
-            choices: ["new", "list", "delete", "quit"]
+            message: " WELLCOME \n 'To Do List' \nWhat would you like to do?\nEnter 'new' to add in List.\nEnter 'list' to display list.\nEnter 'delete' to delete from list.\nEnter 'quit' to close the application",
+            name: 'select',
+            choices: ['new', 'list', 'delete', 'quit']
         }
     ]);
     if (answer.select === "new") {
         let newTodo = await inquirer.prompt([
             {
-                name: "new",
+                message: "Enter new item",
                 type: "input",
-                message: "Enter new item"
+                name: 'new'
             }
         ]);
         todoArray.push(newTodo.new);
-        console.log(chalk.green.bold(`"${newTodo.new}"" added in the list`));
+        console.log(chalk.green.bold.italic(`"${newTodo.new}"" added in the list`));
+    }
+    else if (answer.select === "delete") {
+        let deleteTodo = await inquirer.prompt([
+            {
+                message: "Enter index number you want to delete",
+                type: "input",
+                name: 'delete'
+            }
+        ]);
+        console.log(chalk.blue.bold.italic(`"${todoArray[deleteTodo.delete]}" deleted from the list.`));
+        todoArray.splice(deleteTodo.delete, 1);
+    }
+    else if (answer.select === "list") {
+        console.log(chalk.yellow.bold.italic("================================="));
+        console.log('            "To Do List"');
+        for (let item of todoArray) {
+            console.log(todoArray.indexOf(item) + ": " + item);
+        }
+        if (todoArray[0] === undefined) {
+            console.log("No Entries");
+        }
+        console.log(chalk.yellow.bold.italic("================================="));
+    }
+    if (answer.select === "quit") {
+        console.log(chalk.red.bold.italic(`
+                **************************
+                Thanks for using todo list
+                **************************`));
+        break;
     }
 }
